1aa8b93f698ac4dd1f5e9a014b303294bfc3b24a
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-03-19 17:37:35 +0530

    more colors

--- constants/categoryColors.js
+++ constants/categoryColors.js
@@ -1,49 +1,71 @@
 export default [
-    "#6e0d3d",
-    "#660a20",
-    "#660a0d",
-    "#9e0d0d",
-    "#ff0000",
-    "#ff5500",
-    "#ff9100",
-    "#ffa763",
-    "#ffaa00",
-    "#ffe100",
-    "#e1ff00",
-    "#839117",
-    "#8cff00",
-    "#5d941b",
-    "#11ff00",
-    "#3b941b",
-    "#00ff88",
-    "#1a995e",
-    "#136969",
-    "#255f66",
-    "#134669",
-    "#0fd6cc",
-    "#00aaff",
-    "#0077ff",
-    "#4757ff",
-    "#0037ff",
-    "#121094",
-    "#6e86d4",
-    "#706ed4",
-    "#3d3869",
-    "#353c6e",
-    "#a17aeb",
-    "#6642f5",
-    "#5900ff",
-    "#33256b",
-    "#9447ff",
-    "#9900ff",
-    "#4c1273",
-    "#c894eb",
-    "#f78ff6",
-    "#fa5097",
-    "#f002ec",
-    "#ff0381",
-    "#3d1620",
-    "#472c38",
-    "#313133",
-    "#828282",
-]
+    // Lightest colors (Pastels & Soft Shades)
+    "#fff5e1", // Soft Peach
+    "#ffe6cc", // Light Orange
+    "#ffdd99", // Pale Yellow
+    "#fff7b2", // Soft Butter Yellow
+    "#e8ffd1", // Light Lime Green
+    "#d4fcb1", // Soft Green
+    "#c2fbe1", // Light Teal
+    "#b8eaff", // Light Sky Blue
+    "#d4c2fc", // Lavender
+    "#ffc2e2", // Light Pink
+    "#ffd9e8", // Soft Rose  
+
+    // Brighter colors (Vibrant & Neon Tones)
+    "#ffaa00", // Bright Yellow-Orange
+    "#ffe100", // Strong Yellow
+    "#e1ff00", // Neon Yellow-Green
+    "#b0f300", // Lime Green
+    "#8cff00", // Bright Green
+    "#11ff00", // Pure Green
+    "#00ff88", // Neon Aqua
+    "#0fd6cc", // Cyan Blue
+    "#00aaff", // Vibrant Blue
+    "#0077ff", // Bright Blue
+    "#4757ff", // Bold Royal Blue
+    "#9933ff", // Vibrant Purple  
+
+    // Mid-tones (Neutral & Deep Colors)
+    "#9e0d0d", // Rich Red
+    "#ff0000", // True Red
+    "#ff5500", // Bright Orange
+    "#ff9100", // Sunset Orange
+    "#ffbe00", // Golden Yellow
+    "#5d941b", // Olive Green
+    "#3b941b", // Deep Forest Green
+    "#136969", // Deep Cyan
+    "#134669", // Dark Teal
+    "#33256b", // Deep Indigo
+    "#654ea3", // Medium Purple  
+    "#6e86d4", // Medium Blue
+
+    // Darker colors (Deep, Muted, & Bold Tones)
+    "#6e0d3d", // Dark Burgundy
+    "#660a20", // Deep Red-Brown
+    "#660a0d", // Dark Red
+    "#3d1620", // Dark Maroon
+    "#a1334f", // Dark Ruby
+    "#5c53ad", // Dark Periwinkle
+    "#33256b", // Deep Navy
+    "#491aa1", // Dark Violet
+    "#4c1273", // Dark Purple
+    "#b005ad", // Deep Magenta
+    "#2a1b3d", // Dark Plum
+    "#313133", // Almost Black Gray
+    "#828282", // Dark Gray
+    "#4d4d4d", // Charcoal Gray
+    "#472c38", // Deep Brown
+    "#1f1f1f", // True Black  
+
+    // Extra Shades for More Gradient Options
+    "#ffd1b3", // Light Peach
+    "#ffb3b3", // Soft Coral
+    "#cc99ff", // Soft Purple
+    "#99ccff", // Soft Blue
+    "#99ffcc", // Mint Green
+    "#ffcc99", // Soft Tangerine
+    "#ff99cc", // Rose Pink
+    "#c094ff", // Light Lilac
+    "#aa80ff", // Pastel Purple
+];
