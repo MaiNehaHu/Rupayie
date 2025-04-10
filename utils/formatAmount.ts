c1b95e00e646c91d335b7aa803f401d280f16eb2
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-04-10 11:17:09 +0530

    amount in IN format

--- utils/formatAmount.ts
+++ utils/formatAmount.ts
@@ -1,15 +1,12 @@
 export const formatAmount = (amount: number, currency: any) => {
-  const formattedAmount = !amount
-    ? `0.0`
-    : amount
-        // Ensures two decimal places
-        ?.toFixed(2)
-        // Adds thousand separator
-        .replace(/\d(?=(\d{3})+\.)/g, `$&${currency.thousandSeparator}`)
-        // Replaces decimal separator if needed
-        .replace(".", currency.decimalSeparator);
+  if (!amount) return `${currency.symbol}0.00`;
+
+  const formattedNumber = new Intl.NumberFormat('en-IN', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(amount);
 
   return currency.side === "left"
-    ? `${currency.symbol}${formattedAmount}`
-    : `${formattedAmount}${currency.symbol}`;
+    ? `${currency.symbol}${formattedNumber}`
+    : `${formattedNumber}${currency.symbol}`;
 };
